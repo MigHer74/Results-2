4e7f50aae6bd385ec0cbe6450d475a8c9d46f839
Miguel Hernandez <migher74@outlook.com>
2021-10-24 20:20:42 -0500

    Added change_db, this function runs the sql script (for shorts or long games) in order to update the previous weeks and prepares the database for the new numbers

--- dbaction.py
+++ dbaction.py
@@ -14,6 +14,21 @@ def add_db(a):
     cdb.close()
 
 
+def change_db(selg):
+    cdb = connect_db()
+    cursor_db = cdb.cursor()
+
+    if selg == "N":
+        script_sql = open("script_s.sql")
+    else:
+        script_sql = open("script_l.sql")
+    
+    script_var = script_sql.read()
+    cursor_db.executescript(script_var)
+    cdb.commit()
+    cdb.close()
+
+
 def view_db(vsel):
     cdb = connect_db()
     cursor_db = cdb.cursor()
