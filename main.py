da9f265c992bced49ec6341c897746e9d268c173
Miguel Hernandez <migher74@outlook.com>
2021-10-25 20:58:30 -0500

    Changed the main menu with new options and enter the select_game function

--- main.py
+++ main.py
@@ -6,12 +6,12 @@ cicle = True
 while cicle:
     os.system("cls")
 
-    print("N - Normal Game\nL - Long Game\nE - Enter Previous Data\nV - View Data\nD - Delete All Data\nX - Exit\n")
+    print("N - Normal Game\nP - Play Games\nR - Replay Games\nE - Enter Previous Data\nV - View Data\nD - Delete All Data\nX - Exit\n")
     
     sel = input("Choose an option: ")
 
-    if sel == "N":
-        ms.enter_data("N")
+    if sel == "P":
+        ms.select_game()
     elif sel == "L":
         ms.enter_data("L")
     elif sel == "E":
