8fad7ff71609aa1655c21339eacfb6de7df33eeb
Miguel Hernandez <migher74@outlook.com>
2021-11-02 16:35:05 -0600

    Made correction in the script change the value of idtype field in the UPDATE sentence

--- script_s.sql
+++ script_s.sql
@@ -1,7 +1,7 @@
 DELETE FROM games WHERE idtype = 'A' AND gameweek = 'W6';
 
-UPDATE games SET gameweek = 'W6' WHERE idtype = 1 AND gameweek = 'W5';
-UPDATE games SET gameweek = 'W5' WHERE idtype = 1 AND gameweek = 'W4';
-UPDATE games SET gameweek = 'W4' WHERE idtype = 1 AND gameweek = 'W3';
-UPDATE games SET gameweek = 'W3' WHERE idtype = 1 AND gameweek = 'W2';
-UPDATE games SET gameweek = 'W2' WHERE idtype = 1 AND gameweek = 'W1';
+UPDATE games SET gameweek = 'W6' WHERE idtype = 'A' AND gameweek = 'W5';
+UPDATE games SET gameweek = 'W5' WHERE idtype = 'A' AND gameweek = 'W4';
+UPDATE games SET gameweek = 'W4' WHERE idtype = 'A' AND gameweek = 'W3';
+UPDATE games SET gameweek = 'W3' WHERE idtype = 'A' AND gameweek = 'W2';
+UPDATE games SET gameweek = 'W2' WHERE idtype = 'A' AND gameweek = 'W1';
